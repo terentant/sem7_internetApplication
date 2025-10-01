ff10f75f699d01c4e1d3014eefabd5dc9ca14bb2
terentant <terent.ant@gmail.com>
2025-10-01 12:10:24 +0300

    Update lab1.js

--- lab1.js
+++ lab1.js
@@ -1,16 +1,3 @@
-
-function func(){
-  const arena = new Arena();
-  arena.addWarrior(new Knight("Гром"));
-  arena.addWarrior(new Archer("Тень"));
-  arena.addWarrior(new Warrior("Вихрь"));
-
-  arena.startBattle();
-
-  setTimeout(() => arena.stopBattle(), 20000);
-}
-
-
 function Warrior(name, hp=100){ 
   let warrior = {
     name, 
@@ -23,10 +10,11 @@ function Warrior(name, hp=100){
   return warrior
 }
 
+
 function Knight(name, hp){
   let knight = {
     __proto__ : Warrior(name, hp),
-    takeDamage : (dmg) => Math.floor(dmg*0.7)
+    takeDamage : (dmg) => knight.hp -= Math.floor(dmg*0.7)
   } 
   return knight;
 }
@@ -35,8 +23,8 @@ function Knight(name, hp){
 function Archer(name, hp){
   let archer = {
     __proto__ : Warrior(name, hp),
-    attack : () => {
-      Math.random() > 0.3 ? Math.ceil(Math.random()*20) : 2 * Math.ceil(Math.random()*20)}
+    attack : () => 
+      Math.random() > 0.3 ? Math.ceil(Math.random()*20) : 2 * Math.ceil(Math.random()*20)
   } 
   return archer;
 }
@@ -44,34 +32,46 @@ function Archer(name, hp){
 
 function Arena(){
   let battleId;
+  function battle(){
+    if (arena.warriors.length == 1){
+        console.log(`The winner is ${arena.warriors[0].name}`);
+        arena.stopBattle();
+    }
+    else if(arena.warriors.length == 0){
+        console.log(`Arena is empty!!`);
+        arena.stopBattle();
+    }
+    else{
+        let atackInd = Math.floor(Math.random() * arena.warriors.length);
+        let defenInd = (atackInd + Math.ceil(Math.random() * (arena.warriors.length - 1))) % arena.warriors.length;
+        console.log(`${arena.warriors[atackInd].name} attacking ${arena.warriors[defenInd].name}`);
+        let damag = arena.warriors[atackInd].attack();
+        arena.warriors[defenInd].takeDamage(damag);
+        console.log(`dealt ${damag} damage\n`)
+
+        if(!arena.warriors[defenInd].isAlive()){
+            console.log(`${arena.warriors[defenInd].name} died\n`);
+            arena.warriors.splice(defenInd, 1);
+        }
+    }
+  
+  }
   let arena = {
     warriors : [],
     addWarrior : (w) => arena.warriors.push(w),
-    startBattle : () => {
-      console.log("battle started");
-      battleId = setInterval(() => {
-        if (arena.warriors.length == 1){
-            console.log(`The winner is ${arena.warriors[0].name}`);
-            arena.stopBattle();
-        }
-        else if(arena.warriors.length == 0){
-            console.log(`Arena is empty!!`);
-            arena.stopBattle();
-        }
-        else{
-            let atackInd = Math.floor(Math.random() * arena.warriors.length);
-            let defenInd = (atackInd + Math.floor(Math.random() * (arena.warriors.length - 1))) % arena.warriors.length;
-            arena.warriors[defenInd].takeDamage(arena.warriors[atackInd].attack());
-
-            if(!arena.warriors[defenInd].isAlive()){
-                console.log(`${arena.warriors[defenInd].name} died`);
-                arena.warriors.splice(defenInd, 1);
-            }
-        }
-      },
-      1e5)
-    },
+    startBattle : () => battleId = setInterval(battle, 3000),
     stopBattle : () => clearInterval(battleId) 
   };
   return arena;
 }
+
+
+
+let arena = new Arena();
+arena.addWarrior(new Knight("Гром"));
+arena.addWarrior(new Archer("Тень"));
+arena.addWarrior(new Warrior("Вихрь"));
+
+arena.startBattle();
+
+// setTimeout(() => arena.stopBattle(), 300000);
